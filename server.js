ace2af40c4977e0e353d9955cf1ced2e0e99c85f
nmcanall <nmcanall@citadel.edu>
2020-08-18 20:29:46 -0700

    add a candidate to the database with API call

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -64,16 +66,32 @@ app.delete("/api/candidate/:id", (req, res) => {
     });
 });
 
-// // CREATE a new candidate in the database
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//               VALUES (?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
-// db.run(sql, params, function(err, result) {
-//     if(err) {
-//         console.log(err);
-//     }
-//     console.log(result, this.lastID);
-// });
+// CREATE a new candidate in the database
+app.post("/api/candidate", ({body}, res) => {
+
+    // Check if there are errors in the given candidate object
+    const errors = inputCheck(body, "first_name", "last_name", "industry_connected");
+    if(errors) {
+        res.status(400).json({error: errors});
+        return;
+    }
+
+    // Add given candidate at the end
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                  VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+    db.run(sql, params, function(err, result) {
+        if(err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        res.json({
+            message: "success",
+            data: body,
+            id: this.lastID
+        });
+    });
+});
 
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {

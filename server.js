501d4df94f7b8e0791e2b2101945828c53bfee6b
nmcanall <nmcanall@citadel.edu>
2020-08-18 22:17:23 -0700

    join parties and candidates tables in API call

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ const db = new sqlite3.Database("./db/election.db", err => {
 
 // Pull entire database and console log as an array of objects where each object is a row ('rows' variable)
 app.get("/api/candidates", (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties ON candidates.party_id = parties.id`;
     const params = [];
     db.all(sql, params, (err, rows) => {
         if(err) {
@@ -36,7 +39,11 @@ app.get("/api/candidates", (req, res) => {
 
 // GET a single candidate from the database by reading
 app.get("/api/candidate/:id", (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name
+                 AS party_name
+                 FROM candidates
+                 LEFT JOIN parties ON candidates.party_id = parties.id
+                 WHERE candidates.id = ?`;
     const params = [req.params.id];
     db.get(sql, params, (err, row) => {
         if(err) {

0d502f75db6eb9278b562e572781b0dd6e1d9990
nmcanall <nmcanall@citadel.edu>
2020-08-18 17:05:13 -0700

    add database to server

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// Connect to database
+const db = new sqlite3.Database("./db/election.db", err => {
+    if(err) {
+        return console.error(err.message);
+    }
+    console.log("Connected to the election database.");
+})
+
+// Default response for any other request(Not Found) Catch all
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+// Start server after database connection
+db.on("open", () => {
+    // Listen for API call
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}.`);
+    });
+});
